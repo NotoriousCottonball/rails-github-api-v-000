12f951020f81428cd6c436e09ab6645a2bbe23b1
George  Pianka <gpianka1@gmail.com>
2019-05-07 04:51:59 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   
   root 'repositories#index'
   get '/auth' => 'sessions#create'
-    post '/repositories/create' => 'repositories#create'
-    root 'repositories#index'
+  post '/repositories/create' => 'repositories#create'
+    
 end
